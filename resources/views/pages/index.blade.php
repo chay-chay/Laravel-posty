0b6da70cfc67a242e29805fbade59b00bd0a92ec
Chayanit <chayanit.chaisri@gmail.com>
2021-08-30 16:53:55 -0400

    Added routes & login page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name', 'LSAPP')}}</title>
+</head>
+<body>
+    <h1>Welcome To Laravel</h1>
+    <p>this is laravel application from the "Laravel From Scratch" Youtube sseries</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+// Route::get('/posts', function () {
+//     return view('posts.index');
+// });
+
+// Route::get('/', function () {
+//     return view('posts.index');
+// });
+
+// Route::get('/', function () {
+//     return view('posts.index');
+// });
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@sevices');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
